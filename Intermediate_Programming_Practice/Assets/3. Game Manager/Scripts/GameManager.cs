0c7062cbb31afed03dec39cedd8af057037c4493
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-04-18 17:50:06 +0300

    +added course`s materials

---
+++ Intermediate_Programming_Practice/Assets/3. Game Manager/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // What level the game is currently loaded
+    //Load and unload game level
+    //keep track of game state
+    //generate other persistent systems
+}
