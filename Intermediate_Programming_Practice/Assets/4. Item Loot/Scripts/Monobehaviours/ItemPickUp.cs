34838445ef9c0e92b48d77a7aae96e1ad5e5a723
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-04-20 21:42:58 +0300

    +[Loot] updated ItemPickup for stor and use items

--- Intermediate_Programming_Practice/Assets/4. Item Loot/Scripts/Monobehaviours/ItemPickUp.cs
+++ Intermediate_Programming_Practice/Assets/4. Item Loot/Scripts/Monobehaviours/ItemPickUp.cs
@@ -6,4 +6,84 @@ using UnityEngine;
 public class ItemPickUp : MonoBehaviour
 {
     public ItemPickUps_SO itemDefinition;
+
+    public CharacterStats m_charStats;
+    CharacterInventory m_charInventory;
+
+    GameObject m_foundStats;
+
+    #region Constructors
+    public ItemPickUp()
+    {
+        m_charInventory = CharacterInventory.instance;
+    }
+    #endregion
+
+    private void Start()
+    {
+        if (m_charStats != null)
+        {
+            m_foundStats = GameObject.FindGameObjectWithTag("Player");
+            m_charStats = m_foundStats.GetComponent<CharacterStats>();
+        }
+    }
+
+    void StoreItemInInventory()
+    {
+        m_charInventory.StoreItem(this);
+    }
+
+    public void UseItem()
+    {
+        switch (itemDefinition.itemType)
+        {
+            case ItemTypeDefinitions.HEALTH:
+                {
+                    m_charStats.ApplyHealth(itemDefinition.itemAmount);
+                }
+                break;
+            case ItemTypeDefinitions.MANA:
+                {
+                    m_charStats.ApplyMana(itemDefinition.itemAmount);
+                }
+                break;
+            case ItemTypeDefinitions.WEALTH:
+                {
+                    m_charStats.GiveWealth(itemDefinition.itemAmount);
+                }
+                break;
+            case ItemTypeDefinitions.WEAPON:
+                {
+                    m_charStats.ChangeWeapon(this);
+                }
+                break;
+            case ItemTypeDefinitions.ARMOR:
+                {
+                    m_charStats.ChangeArmor(this);
+                }
+                break;
+            case ItemTypeDefinitions.BUFF:
+                {
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ( other.tag == "Player")
+        {
+            if (itemDefinition.isStorable)
+            {
+                StoreItemInInventory();
+            }
+            else
+            {
+                UseItem();
+            }
+        }
+    }
 }
