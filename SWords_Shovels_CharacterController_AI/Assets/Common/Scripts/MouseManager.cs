7eef5cc6b241520fd8e6a0ae5a3780743a9c4eaa
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-04-20 19:25:11 +0300

    +added moved by mouse click. Realization on unity`s events and nav`s destination

--- SWords_Shovels_CharacterController_AI/Assets/Common/Scripts/MouseManager.cs
+++ SWords_Shovels_CharacterController_AI/Assets/Common/Scripts/MouseManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 
 public class MouseManager : MonoBehaviour
@@ -8,6 +9,8 @@ public class MouseManager : MonoBehaviour
     public Texture2D pointer; // normal mouse pointer
     public Texture2D target; // target mouse pointer
     public Texture2D doorway; // doorway mouse pointer
+
+    public EventVector3 m_OnClickEnvironment;
 
     void Update()
     {
@@ -26,7 +29,10 @@ public class MouseManager : MonoBehaviour
                 Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
             }
 
-
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_OnClickEnvironment.Invoke(hit.point);
+            }
         }
         else
         {
@@ -35,3 +41,5 @@ public class MouseManager : MonoBehaviour
     }
 }
 
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
